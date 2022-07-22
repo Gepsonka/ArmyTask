3e7f5237d8edf89ff0053ed5a01fced5d288bb4d
Botond Miller <gepsonka@pop-os.localdomain>
2022-07-23 01:35:55 +0200

    install sript

---
+++ install.sh
@@ -0,0 +1,104 @@
+#!/bin/bash
+
+echo "================================================"
+echo "Django Car database apllication by Botond Molnar"
+echo "================================================"
+echo "Version: 1.0.0-RELEASE"
+echo "======================"
+
+echo "==============================="
+echo "Installing python virtualenv..."
+echo "==============================="
+
+if python3 -m pip install --user virtualenv; then
+    echo "python3 virtualenv was successfully created"
+else 
+    echo "Could not install python3 virtualenv :("
+    echo "Check if the script has correct rights and you have internet connection."
+    exit 0
+fi
+
+if python3 -m venv ./venv; then
+    echo "Venv was created successfully"
+else 
+    echo "Could not create venv :("
+    exit 0
+fi
+
+current_dir = `pwd`
+venv_path = "/venv/source/activate"
+
+if source $current_dir$venv_path; then
+    echo "venv started successfully"
+else 
+    echo "Could not start venv :("
+    exit 0
+fi
+
+
+echo "=========================="
+echo "Installing dependencies..."
+echo "=========================="
+
+if python3 -m pip install requirements.txt; then
+    echo "Dependencies are installed."
+else 
+    echo "Could not install dependencies :("
+    exit 0
+
+echo "====================="
+echo "Creating .env file..."
+echo "====================="
+
+echo "DJANGO_SECRET=-7huzqi_15%d3t+oprzw0%bdh\$!x&lnad2u6(ac27jx4a*#sp%" > ./CarDatabase/.env
+
+
+echo "====================="
+echo "Migrating database..."
+echo "====================="
+
+python3 $current_dir/CarDatabase/manage.py makemigrations
+python3 $current_dir/CarDatabase/manage.py migrate
+
+echo "====================="
+echo "Creating superuser..."
+echo "====================="
+
+read -p 'Username: ' uservar
+read -sp 'Password: ' passwdvar
+
+cd CarDatabase
+read -d '' python_cmd << EOF
+from CustomUser.models import CustomUser
+CustomUser.objects.create_superuser(
+    username = '$uservar',
+    email = 'a@b.com',
+    password = '$passwdvar',    
+).save()
+EOF
+
+if python3 -c "$python_cmd"; then
+    echo ""
+else
+    echo "Could not create superuser :("
+    exit 0
+fi
+
+echo "==================================="
+echo "Superuser was successfully created!"
+echo "==================================="
+echo ""
+echo ""
+
+echo "Install process finished. The first thing you should do"
+echo "is changing DJANGO_SECRET in the .env file in the CarDatabase folder."
+echo "You can generate django secret on this site:"
+echo "https://djecrety.ir/"
+echo "===================================================================="
+echo "you can start the server with the runserver.sh script"
+
+echo "Have fun examining the application :D!"
+
+
+
+
---
+++ runserver.sh
@@ -0,0 +1,4 @@
+echo "If you do not give port it will be automatically 8000"
+read -p "Port" port 
+
+python3 CarDatabase/manage.py runserver $port
