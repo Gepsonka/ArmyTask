4be46b0e1a9202bd38ffc6273bd4cefd156eb690
Botond Miller <gepsonka@pop-os.localdomain>
2022-07-23 18:37:20 +0200

    bugfixes

--- runserver.sh
+++ runserver.sh
@@ -1,4 +1,4 @@
-echo "If you do not give port it will be automatically 8000"
+echo "If you leave the port empty it will automatically be 8000."
 read -p "Port: " port 
 
 python3 CarDatabase/manage.py runserver $port
