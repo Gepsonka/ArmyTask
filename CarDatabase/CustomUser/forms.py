ed245e12ed419ce8df7bc8ea568d97d7ff5990cc
Botond Miller <gepsonka@pop-os.localdomain>
2022-07-24 16:54:03 +0200

    Fixing some inaccuracies

--- CarDatabase/CustomUser/forms.py
+++ CarDatabase/CustomUser/forms.py
@@ -1,12 +1,15 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
+from django.core.validators import MinLengthValidator, MaxLengthValidator
+
 
 from .models import CustomUser
 
 class UserRegisterForm(UserCreationForm):
+    username = forms.CharField(required=True, min_length=6, validators=[MinLengthValidator(6), MaxLengthValidator(16)])
     email = forms.EmailField()
 
     class Meta:
         model = CustomUser
-        fields = ['first_name', 'last_name','username', 'email', 'password1', 'password2']
+        fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2']
         
--- CarDatabase/CustomUser/models.py
+++ CarDatabase/CustomUser/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import MinLengthValidator, MaxLengthValidator
 # Create your models here.
 
 class CustomUser(AbstractUser):
+    username = models.CharField(max_length=16, validators=[MinLengthValidator(6), MaxLengthValidator(16)], unique=True)
     email = models.EmailField(unique=True)
     unsuccessful_attempts = models.SmallIntegerField(default=0)
     requested_delete = models.BooleanField(default=False)
